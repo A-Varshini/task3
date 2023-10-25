0dc3342f227557058d9d7310b726dde591e059e5
A-Varshini <varshupretty04@gmail.com>
2023-10-26 00:23:45 +0530

    first commit

---
+++ src/Colorpicker.js
@@ -0,0 +1,40 @@
+
+import React, { Component } from 'react';
+
+
+class ColorPicker extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+          color: '#1569a8',
+          active: false
+        };
+      }
+      
+      handleChange = (e) => {
+        this.setState({
+          oldColor: this.state.color,
+          color: e.target.value,
+          active: !this.state.active,
+        });
+      };
+      
+      render() {
+        return <div className="background" style={{ background: this.state.active ? this.state.oldColor : this.state.color }} >
+          <div className={this.state.active ? 'overlay active' : 'overlay'} style={{ background: this.state.active ? this.state.color : this.state.oldColor }} />
+          <h1 className="title">Pickk a Colorr!!! 🥳</h1>
+          <label className="color-selector">
+            <span className="circle" style={{ background: this.state.color }} />
+            <span>{this.state.color}</span>
+            <input
+              type="color"
+              value={this.state.color}
+              onChange={this.handleChange}
+              className="hidden"
+             />
+          </label>
+        </div>;
+      }
+}
+
+export default ColorPicker;
